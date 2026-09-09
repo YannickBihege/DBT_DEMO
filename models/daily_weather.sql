61f09ed5e6d487a703462e1ef6b8934cc00fa529
Yann Yann <yannoftherain@gmail.com>
2026-09-09 14:04:17 +0000

    daily weather

---
+++ models/daily_weather.sql
@@ -0,0 +1,33 @@
+with daily_weather as (
+    select 
+    date(time) as daily_weather,
+    weather,
+    temp,
+    pressure,
+    humidity,
+    clouds
+    from {{ source('demo', 'weather') }}
+    limit 10
+)
+
+,
+ daily_weather_agg as(
+
+    select 
+    daily_weather,
+    weather,
+    avg(temp) as avg_temp,
+    ROUND(avg(pressure),2) as avg_pressure,
+    avg(humidity) as avg_humidity,
+    avg(clouds) as avg_clouds,
+
+    count (weather) ,
+    row_number() over (PARTITION by daily_weather order by (count(weather)) desc ) AS row_number
+    from daily_weather
+    group by daily_weather, weather
+
+    -- you cannot use where therefore
+    qualify  row_number() over (PARTITION by daily_weather order by (count(weather)) desc )  = 1
+ )
+
+select * from daily_weather_agg
